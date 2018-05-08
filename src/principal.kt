cb88fe7ed2b258ed036808eb302301a967e429f1
Avill2 <andrea.villacis@epn.edu.ec>
2018-05-08 10:40:52 -0500

    realizado cambios

--- src/principal.kt
+++ src/principal.kt
@@ -1,59 +1,48 @@
 
-
 fun main(args: Array<String>){
+    modeloAuto.tipoAuto.add("sail")
+    modeloAuto.tipoAuto.add("cruz")
+    modeloAuto.tipoAuto.add("aveo")
+    modeloAuto.tipoAuto.add("Luv")
+
     println("FINANCIAMIENTO DE UN AUTO")
 
-    fun ivaDocePorciento(){
+    fun ivaDocePorciento(): Int {
         val numero=12
         val iva=numero/100;
+        return iva
     }
      var financiamiento="fideicomiso"
 
-    class modeloAuto{
-        companion object {
-            val tipoAuto: ArrayList<String> = ArrayList()
-            fun recuperarIndice(indice:Int): String? {
-                for (indiceArreglo in tipoAuto.indices){
-                    if (indiceArreglo==indice){
-                        return tipoAuto[indiceArreglo]
-                    }
-                }
-                return null
-            }
-        }
-    }
-
-    modeloAuto.tipoAuto.add("Chevrolet SAIL")
-    modeloAuto.tipoAuto.add("Chevrolet Luv")
 
     when (financiamiento){
 
         "fideicomiso" -> {
             println("Usted a escogido el plan fideicomiso")
             println("ingrese el valor del auto")
-            var valorInicial = readLine()?.toInt()
-            println("ingrese el interes")
-            var interes = readLine()?.toInt()
-            println("ingrese el plazo en meses")
-            var plazo = readLine()?.toInt()
-            val interesTotal=interes/100
-            val total=((valorInicial*(interesTotal))+(valorInicial*ivaDocePorciento())+valorInicial)
+            var valorInicial =5000
+            //println("ingrese el interes")
+            var interes = 0.1608
+            //println("ingrese el plazo en meses")
+            var plazo = 48
+            //val interesTotal=interes/100
+            val total=((valorInicial*(interes))+(valorInicial*ivaDocePorciento())+valorInicial)
             println("el valor a pagar de su auto es $total")
             val cuotaMensual= total/plazo
             println("el valor mensual a pagar es $cuotaMensual")
+            println("Su auto de marca "+modeloAuto.recuperarIndice(indice = 1 ) + ": La cuota ChevyPlan es $cuotaMensual y al final del plazo pagara $total" )
         }
 
         else ->{
             println("El plan que ha escogido es CHEVYPLAN")
-            println("ingrese el valor del auto")
-            var valorInicial = readLine()?.toInt()
-            println("ingrese el plazo en meses")
-            var plazo = readLine()?.toInt()
+            //println("ingrese el valor del auto")
+            var valorInicial = 5000
+            //println("ingrese el plazo en meses")
+            var plazo = 48
             val totalChevi=(valorInicial*ivaDocePorciento()+valorInicial)
             println("el valor a pagar de su auto es $totalChevi")
             val cuotaChevy=totalChevi/plazo
-            println("La cuota ChevyPlan es $cuotaChevy y al final del pazo pagara $totalChevi" )
-
+            println("Su auto de marca "+modeloAuto.recuperarIndice(indice = 2 ) + ": La cuota ChevyPlan es $cuotaChevy y al final del plazo pagara $totalChevi" )
 
         }
     }
